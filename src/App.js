fde90e339087063bee5510379305a5cd3602e85f
Mehran <mehranpanahi79@gmail.com>
2023-09-02 10:19:24 +0330

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./assets/main.css";
 function App() {
   return (
     <div>
-      <h1 className="text-6xl font-extrabold shadow-lg text-blue-600">
+      <h1 className="text-6xl font-extrabold shadow-lg text-blue-600 text-center">
         Hello World
       </h1>
     </div>
