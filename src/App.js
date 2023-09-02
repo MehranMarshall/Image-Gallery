b0a30221e42b217b5a50dcb7396afae7daa299c9
Mehran <mehranpanahi79@gmail.com>
2023-09-02 10:16:42 +0330

    updated

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-function App() {}
+import React from "react";
+import "./assets/main.css";
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl font-extrabold shadow-lg text-blue-600">
+        Hello World
+      </h1>
+    </div>
+  );
+}
 
 export default App;
