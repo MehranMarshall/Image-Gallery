834f04610dd89ebaff725e1ced09d4e78364a03e
MehranMarshall <mehranpanahi79@gmail.com>
2023-09-03 10:15:35 +0330

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ImageCard } from "./Components/ImageCard";
 import "./assets/main.css";
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("Football");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.PIXABAY_API}&q=${term}&image_type=photo`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <div className="max-w-sm shadow-lg rounded overflow-hidden p-3">
-      <img src="https://source.unsplash.com/random" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by John Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            400
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            140
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4 ">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
+    <div className="container mx-auto ">
+      <div className="gird grid-cols-3 gap-4">
+        {images.map((image) => {
+          <ImageCard key={image.id} image={image} />;
+        })}
       </div>
     </div>
   );
