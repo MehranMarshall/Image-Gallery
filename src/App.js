5677795405bd756e1c81078b727b7618333358af
Mehran <mehranpanahi79@gmail.com>
2023-09-05 09:19:11 +0330

    page showed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./assets/main.css";
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [term, setTerm] = useState("Football");
+  const [term, setTerm] = useState("");
 
   useEffect(() => {
     fetch(`https://picsum.photos/v2/list?page=2&limit=100`)
@@ -16,10 +16,10 @@ function App() {
       .catch((err) => console.log(err));
   }, []);
   return (
-    <div className="mx-auto ">
+    <div className="mx-auto">
       <div className="gird grid-cols-3 gap-4">
         {images.map((image) => {
-          <ImageCard key={image.id} image={image} />;
+          return <ImageCard image={image} />;
         })}
       </div>
     </div>
